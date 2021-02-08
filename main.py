9547951bf91fa767379bb371330b4055a5ab648d
Artyom Rodionov <72665171+zzzondy@users.noreply.github.com>
2021-02-08 15:42:57 +0300

    Второй пункт
    2

---
+++ main.py
@@ -0,0 +1,78 @@
+import os
+import requests
+import pygame
+
+toponym_to_find = 'Австралия'
+
+geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+
+geocoder_params = {
+    "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+    "geocode": toponym_to_find,
+    "format": "json"}
+
+response = requests.get(geocoder_api_server, params=geocoder_params)
+
+if not response:
+    pass
+
+json_response = response.json()
+toponym = json_response["response"]["GeoObjectCollection"][
+    "featureMember"][0]["GeoObject"]
+toponym_coodrinates = toponym["Point"]["pos"]
+toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
+delta = '18'
+
+
+def change_scale(plus): # второй этап
+    global delta
+    delta = float(delta)
+    if plus:
+        delta *= 2
+    else:
+        delta /= 2
+
+
+def change_map():
+    global map_params, map_api_server, response, map_file
+    map_params = {
+        "ll": ",".join([toponym_longitude, toponym_lattitude]),
+        "spn": ",".join([delta, delta]),
+        "l": "map"
+    }
+
+    map_api_server = "http://static-maps.yandex.ru/1.x/"
+    response = requests.get(map_api_server, params=map_params)
+    map_file = "map.png"
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+
+
+map_params = {
+    "ll": ",".join([toponym_longitude, toponym_lattitude]),
+    "spn": ",".join([delta, delta]),
+    "l": "map"
+}
+
+map_api_server = "http://static-maps.yandex.ru/1.x/"
+response = requests.get(map_api_server, params=map_params)
+map_file = "map.png"
+with open(map_file, "wb") as file:
+    file.write(response.content)
+
+pygame.init()
+screen = pygame.display.set_mode((600, 450))
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.K_PAGEUP:
+            change_scale(True)
+        if event.type == pygame.K_PAGEDOWN:
+            change_scale(False)
+    screen.blit(pygame.image.load(map_file), (0, 0))
+    pygame.display.flip()
+pygame.quit()
+
+os.remove(map_file)
